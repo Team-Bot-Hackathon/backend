184777cc57cf5a4e9aac51a3171a82868f922346
Samarth Naphade <samarthnaphade@gmail.com>
2021-11-17 19:41:48 +0530

    [feature]Added SignIn Router for Pharmacy

--- router/signInRouter.js
+++ router/signInRouter.js
@@ -62,14 +62,53 @@ signInRouter.route("/user")
     });
 
 signInRouter.route('/pharmacy')
-    .post((req,res) => {
-        user_name = req.body.user_name;
-        password = req.body.password;
-        if(!user_name || !password){
-            res.send({"signedUp":false,"error":"pls send vaild username and password"});
-        }else{
-            
-        }
-    })
+.post((req,res) => {
+    user_name = req.body.user_name;
+    password = req.body.password;
+    if(!user_name || !password){
+        res.send({"signedUp":false,"error":"pls send vaild username and password"});
+    }else{
+        connection.getConnection((err,con) => {
+            if(err) res.send(err);
+            else{
+                con.query(`SELECT * FROM pharmacy WHERE user_name='${user_name}'`,(err,result) => {
+                    if(!result[0]){
+                        res.send({
+                            "signedIn": false,
+                            "type": "pharmacy",
+                            "token": "",
+                            "err": "Invalid Credentails"
+                        })
+                    }else{
+                        bcrypt.compare(password,result[0].password,(err,matched) => {
+                            if(err) res.send(err);
+                            else{
+                                if(matched){
+                                    jwt.sign({pharmacy_id: result[0].pharmacy_id}, process.env.KEY,{expiresIn: '1d'},(err,token) => {
+                                        if(err) res.send(err)
+                                        else{
+                                            res.send({
+                                                "signedIn": true,
+                                                "type": "pharmacy",
+                                                "token": token
+                                            });
+                                        }
+                                    })
+                                }else{
+                                    res.send({
+                                        "signedIn": false,
+                                        "type": "pharmacy",
+                                        "token": "",
+                                        "err": "Invalid Credentails"
+                                    });
+                                }
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    }
+});
 
 module.exports=signInRouter;
