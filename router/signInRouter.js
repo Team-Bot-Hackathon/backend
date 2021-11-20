03caf4f48777dd0771e2f34951a37e4538bb4077
Samarth Naphade <samarthnaphade@gmail.com>
2021-11-20 21:28:01 +0530

    [fix]Added exception handling in the sign In Router

--- router/signInRouter.js
+++ router/signInRouter.js
@@ -13,6 +13,63 @@ const saltRounds = 10;
 
 signInRouter.route("/user")
     .post((req,res) => {
+        try{
+            user_name = req.body.user_name;
+            password = req.body.password;
+            if(!user_name || !password){
+                res.send({"signedUp":false,"error":"pls send vaild username and password"});
+            }else{
+                connection.getConnection((err,con) => {
+                    if(err) res.send(err);
+                    else{
+                        con.query(`SELECT * FROM user WHERE user_name='${user_name}'`,(err,result) => {
+                            if(!result[0]){
+                                res.send({
+                                    "signedIn": false,
+                                    "type": "user",
+                                    "token": "",
+                                    "err": "Invalid Credentails"
+                                })
+                            }else{
+                                bcrypt.compare(password,result[0].password,(err,matched) => {
+                                    if(err) res.send(err);
+                                    else{
+                                        if(matched){
+                                            jwt.sign({user_id: result[0].user_id}, process.env.KEY,{expiresIn: '1d'},(err,token) => {
+                                                if(err) res.send(err)
+                                                else{
+                                                    res.send({
+                                                        "signedIn": true,
+                                                        "type": "user",
+                                                        "token": token
+                                                    });
+                                                    con.release();
+                                                }
+                                            })
+                                        }else{
+                                            res.send({
+                                                "signedIn": false,
+                                                "type": "user",
+                                                "token": "",
+                                                "err": "Invalid Credentails"
+                                            });
+                                            con.release();
+                                        }
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+        }catch(err){
+            res.send(err);
+        }
+    });
+
+signInRouter.route('/pharmacy')
+.post((req,res) => {
+    try{
         user_name = req.body.user_name;
         password = req.body.password;
         if(!user_name || !password){
@@ -21,11 +78,11 @@ signInRouter.route("/user")
             connection.getConnection((err,con) => {
                 if(err) res.send(err);
                 else{
-                    con.query(`SELECT * FROM user WHERE user_name='${user_name}'`,(err,result) => {
+                    con.query(`SELECT * FROM pharmacy WHERE user_name='${user_name}'`,(err,result) => {
                         if(!result[0]){
                             res.send({
                                 "signedIn": false,
-                                "type": "user",
+                                "type": "pharmacy",
                                 "token": "",
                                 "err": "Invalid Credentails"
                             })
@@ -34,25 +91,23 @@ signInRouter.route("/user")
                                 if(err) res.send(err);
                                 else{
                                     if(matched){
-                                        jwt.sign({user_id: result[0].user_id}, process.env.KEY,{expiresIn: '1d'},(err,token) => {
+                                        jwt.sign({pharmacy_id: result[0].pharmacy_id}, process.env.KEY,{expiresIn: '1d'},(err,token) => {
                                             if(err) res.send(err)
                                             else{
                                                 res.send({
                                                     "signedIn": true,
-                                                    "type": "user",
+                                                    "type": "pharmacy",
                                                     "token": token
                                                 });
-                                                con.release();
                                             }
                                         })
                                     }else{
                                         res.send({
                                             "signedIn": false,
-                                            "type": "user",
+                                            "type": "pharmacy",
                                             "token": "",
                                             "err": "Invalid Credentails"
                                         });
-                                        con.release();
                                     }
                                 }
                             })
@@ -61,55 +116,8 @@ signInRouter.route("/user")
                 }
             })
         }
-    });
-
-signInRouter.route('/pharmacy')
-.post((req,res) => {
-    user_name = req.body.user_name;
-    password = req.body.password;
-    if(!user_name || !password){
-        res.send({"signedUp":false,"error":"pls send vaild username and password"});
-    }else{
-        connection.getConnection((err,con) => {
-            if(err) res.send(err);
-            else{
-                con.query(`SELECT * FROM pharmacy WHERE user_name='${user_name}'`,(err,result) => {
-                    if(!result[0]){
-                        res.send({
-                            "signedIn": false,
-                            "type": "pharmacy",
-                            "token": "",
-                            "err": "Invalid Credentails"
-                        })
-                    }else{
-                        bcrypt.compare(password,result[0].password,(err,matched) => {
-                            if(err) res.send(err);
-                            else{
-                                if(matched){
-                                    jwt.sign({pharmacy_id: result[0].pharmacy_id}, process.env.KEY,{expiresIn: '1d'},(err,token) => {
-                                        if(err) res.send(err)
-                                        else{
-                                            res.send({
-                                                "signedIn": true,
-                                                "type": "pharmacy",
-                                                "token": token
-                                            });
-                                        }
-                                    })
-                                }else{
-                                    res.send({
-                                        "signedIn": false,
-                                        "type": "pharmacy",
-                                        "token": "",
-                                        "err": "Invalid Credentails"
-                                    });
-                                }
-                            }
-                        })
-                    }
-                })
-            }
-        })
+    }catch(err){
+        res.send(err);
     }
 });
 
