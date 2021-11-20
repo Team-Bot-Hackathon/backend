c25f783ca6d189654ad59b034d2bcb7da2592094
Samarth Naphade <samarthnaphade@gmail.com>
2021-11-20 21:46:51 +0530

    [fix]Added commnets in the signIn Router

--- router/signInRouter.js
+++ router/signInRouter.js
@@ -14,6 +14,8 @@ const saltRounds = 10;
 signInRouter.route("/user")
     .post((req,res) => {
         try{
+
+            //parse the user_name and password from the body
             user_name = req.body.user_name;
             password = req.body.password;
             if(!user_name || !password){
@@ -22,7 +24,10 @@ signInRouter.route("/user")
                 connection.getConnection((err,con) => {
                     if(err) res.send(err);
                     else{
+
                         con.query(`SELECT * FROM user WHERE user_name='${user_name}'`,(err,result) => {
+
+                            //check if given username does not exists 
                             if(!result[0]){
                                 res.send({
                                     "signedIn": false,
@@ -31,10 +36,14 @@ signInRouter.route("/user")
                                     "err": "Invalid Credentails"
                                 })
                             }else{
+                                
+                                //compare the password of given by the user and hash present in the database to check the authenticity of the user
                                 bcrypt.compare(password,result[0].password,(err,matched) => {
                                     if(err) res.send(err);
                                     else{
                                         if(matched){
+
+                                            //If password match's then generate a JWT token and send to the client side 
                                             jwt.sign({user_id: result[0].user_id}, process.env.KEY,{expiresIn: '1d'},(err,token) => {
                                                 if(err) res.send(err)
                                                 else{
@@ -47,6 +56,8 @@ signInRouter.route("/user")
                                                 }
                                             })
                                         }else{
+
+                                            //If password does not match then notify the client side that authentication failed
                                             res.send({
                                                 "signedIn": false,
                                                 "type": "user",
@@ -71,6 +82,8 @@ signInRouter.route("/user")
 signInRouter.route('/pharmacy')
 .post((req,res) => {
     try{
+
+        //parse the user_name and password from the body
         user_name = req.body.user_name;
         password = req.body.password;
         if(!user_name || !password){
@@ -80,6 +93,8 @@ signInRouter.route('/pharmacy')
                 if(err) res.send(err);
                 else{
                     con.query(`SELECT * FROM pharmacy WHERE user_name='${user_name}'`,(err,result) => {
+
+                        //check if given username does not exists 
                         if(!result[0]){
                             res.send({
                                 "signedIn": false,
@@ -88,6 +103,8 @@ signInRouter.route('/pharmacy')
                                 "err": "Invalid Credentails"
                             })
                         }else{
+                            
+                            //compare the password of given by the user and hash present in the database to check the authenticity of the user 
                             bcrypt.compare(password,result[0].password,(err,matched) => {
                                 if(err) res.send(err);
                                 else{
@@ -95,6 +112,8 @@ signInRouter.route('/pharmacy')
                                         jwt.sign({pharmacy_id: result[0].pharmacy_id}, process.env.KEY,{expiresIn: '1d'},(err,token) => {
                                             if(err) res.send(err)
                                             else{
+
+                                                //If password match's then generate a JWT token and send to the client side 
                                                 res.send({
                                                     "signedIn": true,
                                                     "type": "pharmacy",
@@ -103,6 +122,8 @@ signInRouter.route('/pharmacy')
                                             }
                                         })
                                     }else{
+                                        
+                                        //If password does not match then notify the client side that authentication failed
                                         res.send({
                                             "signedIn": false,
                                             "type": "pharmacy",
