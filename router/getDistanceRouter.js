ea6c566822b85dd98e6e32d553d83fd96fbf3079
Samarth Naphade <samarthnaphade@gmail.com>
2021-11-20 21:22:56 +0530

    [fix]Added exception handling in the get Distance Route

--- router/getDistanceRouter.js
+++ router/getDistanceRouter.js
@@ -31,74 +31,78 @@ function toRad(Value)
 
 getDistanceRouter.route("/")
     .post((req,res) => {
-        lat1 = req.body.lat1;
-        lat2 = req.body.lat2;
-        lon1 = req.body.lon1;
-        lon2 = req.body.lon2;
-        var options = {
-            'method': 'POST',
-            'url': 'http://www.mapquestapi.com/directions/v2/route?key='+process.env.API_KEY,
-            'headers': {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "locations": [
-                    ""+lat1+", "+lon1+"",
-                    ""+lat2+", "+lon2+""
-                ],
-                "options": {
-                    "avoids": [],
-                    "avoidTimedConditions": false,
-                    "doReverseGeocode": false,
-                    "shapeFormat": "raw",
-                    "generalize": 0,
-                    "routeType": "fastest",
-                    "timeType": 1,
-                    "locale": "en_US",
-                    "unit": "k",
-                    "enhancedNarrative": false,
-                    "drivingStyle": 2,
-                    "highwayEfficiency": 21
-                }
-            })
-        };
+        try{
+            lat1 = +(req.body.lat1);
+            lat2 = +(req.body.lat2);
+            lon1 = +(req.body.lon1);
+            lon2 = +(req.body.lon2);
+            var options = {
+                'method': 'POST',
+                'url': 'http://www.mapquestapi.com/directions/v2/route?key='+process.env.API_KEY,
+                'headers': {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "locations": [
+                        ""+lat1+", "+lon1+"",
+                        ""+lat2+", "+lon2+""
+                    ],
+                    "options": {
+                        "avoids": [],
+                        "avoidTimedConditions": false,
+                        "doReverseGeocode": false,
+                        "shapeFormat": "raw",
+                        "generalize": 0,
+                        "routeType": "fastest",
+                        "timeType": 1,
+                        "locale": "en_US",
+                        "unit": "k",
+                        "enhancedNarrative": false,
+                        "drivingStyle": 2,
+                        "highwayEfficiency": 21
+                    }
+                })
+            };
 
-        if(calculateDistance(lat1,lon1,lat2,lon2) > 100){
-            payload = {
-                route:{
-                    distance: calculateDistance(lat1,lon1,lat2,lon2)
+            if(calculateDistance(lat1,lon1,lat2,lon2) > 100){
+                payload = {
+                    route:{
+                        distance: calculateDistance(lat1,lon1,lat2,lon2)
+                    }
                 }
+                body= payload;
+                res.send(body);
+            }else{
+                request(options, function (error, response) {
+                    if (error) res.send(error);
+                    else{
+                        body = JSON.parse(response.body);
+                        if(body['fault']){
+                            payload = {
+                                route:{
+                                    distance: calculateDistance(lat1,lon1,lat2,lon2)
+                                }
+                            }
+                            body= payload;
+                            res.send(body);
+                            
+                        }
+                        else if(body['route']['routeError']['errorCode'] == -400){
+                            res.send(body);
+                        }else{
+                            payload = {
+                                route:{
+                                    distance: calculateDistance(lat1,lon1,lat2,lon2)
+                                }
+                            }
+                            body=payload;
+                            res.send(body);
+                        }
+                    }
+                });
             }
-            body= payload;
-            res.send(body);
-        }else{
-            request(options, function (error, response) {
-                if (error) res.send(error);
-                else{
-                    body = JSON.parse(response.body);
-                    if(body['fault']){
-                        payload = {
-                            route:{
-                                distance: calculateDistance(lat1,lon1,lat2,lon2)
-                            }
-                        }
-                        body= payload;
-                        res.send(body);
-                        
-                    }
-                    else if(body['route']['routeError']['errorCode'] == -400){
-                        res.send(body);
-                    }else{
-                        payload = {
-                            route:{
-                                distance: calculateDistance(lat1,lon1,lat2,lon2)
-                            }
-                        }
-                        body=payload;
-                        res.send(body);
-                    }
-                }
-            });
+        }catch(err){
+            res.send(err);
         }
 
     });
